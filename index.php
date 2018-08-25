25df02d2ee8629776610e6d1983d362928b0e6b5
DenisYakupov <41899918+DenisYakupov@users.noreply.github.com>
2018-08-25 15:25:32 +0300

    Update index.php

--- index.php
+++ index.php
@@ -28,41 +28,43 @@ class Test {
     public function getUniqUrls($url)
     {
         $this->urls = count(array_count_values($this->matches["$url"]));
+        return $this;
     }
 
     //Суммируем объем трафика
     public function getSumTraffic($traffic)
     {
         $this->traffic = array_sum($this->matches["$traffic"]);
+        return $this;
     }
 
     //Считаем количество запросов от поисковиков
     public function getCrawlers($bots)
     {
         $this->crawlers = array_count_values(array_diff($this->matches["$bots"], array('')));
+        return $this;
     }
 
     //Подсчитываем коды ответов
     public function getStatusCodes($codes)
     {
         $this->statusCodes = array_count_values($this->matches["$codes"]);
+        return $this;
     }
 
     //Подсчитываем колличесво просмотров
     public function getViews($views)
     {
         $this->views = count($this->matches["$views"]);
+        return $this;
     }
 }
 
 $pattern = '/^([\d\.]+)[\s|-]+\[[\S ]+\][ "\w]+([\/a-z]+[\S\s]+?)"\s(\d+)[-\s]+(\d+)[\S\s]+?"[\s\S]+?\)[\s]?(Google|Bing|Yandex|Baidu)*.*$/m';
 
 $obj = new Test($pattern,'access.log');
-$obj->getViews(1);
-$obj->getUniqUrls(2);
-$obj->getSumTraffic(4);
-$obj->getCrawlers(5);
-$obj->getStatusCodes(3);
+$obj->getViews(1)->getUniqUrls(2)->getSumTraffic(4)->getCrawlers(5)->getStatusCodes(3);
+
 
 
 debug(json_encode($obj, JSON_PRETTY_PRINT));
